d75c2eaebe13a76916305a73f3c2c714818f7bfc
Yash-Ralhi <mishrayash2015@gmail.com>
2024-11-26 16:39:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function enterChatRoom() {
         document.getElementById("chatRoom").style.display = "block";
 
         // Connect to WebSocket server
-        socket = new WebSocket("ws://https://chat-application-web.onrender.com");
+        socket = new WebSocket("ws://chat-application-web.onrender.com");
 
         socket.onopen = function() {
             console.log("Connected to the server");
